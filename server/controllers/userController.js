e395bd3f65b8719cf7bec692fc20a4cd59563398
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-10-26 13:51:09 +0300

    add delete user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,23 +11,43 @@ const getUsers = asyncHandler(async (req, res) => {
 const getUser = asyncHandler(async (req, res) => {
   const id = req.params.id;
   const user = await User.findById(id);
+  if (!user) {
+    res.status(400);
+    throw new Error("User Not Found");
+  }
   res.status(200).json(user);
 });
 
 // create user
 const createUser = asyncHandler(async (req, res) => {
-  const name = req.body.name;
-  if (!name) {
+  const { name, email, password, bio } = req.body;
+  if (!name || !email || !password) {
     res.status(400);
-    throw new Error("Please add a name");
+    throw new Error("Please add a name, email and password");
   }
-
+  // create a user
   const createUser = await User.create({
-    name: name,
+    name,
+    email,
+    password,
+    bio,
   });
   if (createUser) {
     res.status(201).json(createUser);
   }
 });
 
-export { getUsers, getUser, createUser };
+// delete
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const user = await User.deleteOne({ _id: id });
+  if (!user) {
+    res.status(400);
+    throw new Error("User Not Found");
+  }
+  res.status(200).json({
+    success: true,
+  });
+});
+
+export { getUsers, getUser, createUser, deleteUser };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 const router = Router();
-import { createUser, getUser, getUsers } from "../controllers/userController.js";
+import {
+  createUser,
+  getUser,
+  getUsers,
+  deleteUser,
+} from "../controllers/userController.js";
 
 // create crud routes
 router.route("/").post(createUser).get(getUsers);
-router.route("/:id").get(getUser);
+router.route("/:id").get(getUser).delete(deleteUser);
 
 export default router;
