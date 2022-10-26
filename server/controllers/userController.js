5811555d5484663b0642e39fd4af11bf81dc3ac6
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-10-26 21:27:32 +0300

    register a user with hashed passwords

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,58 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-// @desc get users
-// @route GET /api/users
-// @access public
-const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find();
-  res.status(200).json(users);
+// @desc  register a user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, bio } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add name, email and password");
+  }
+  // check if user is already registred
+  const userExists = await User.findOne({ email: email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists, Login");
+  }
+
+  // hash user passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create a user
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    bio,
+  });
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      bio: newUser.bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
 });
 
-// @desc get a user
-// @route GET /api/users:id
-// @access private
-const getUser = asyncHandler(async (req, res) => {
-  const id = req.params.id;
-  const user = await User.findById(id);
+// @desc login a user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add email and password");
+  }
+  const user = await User.findOne({ email: email }); //find user by email
   if (!user) {
     res.status(400);
     throw new Error("User Not Found");
@@ -22,29 +60,21 @@ const getUser = asyncHandler(async (req, res) => {
   res.status(200).json(user);
 });
 
-// @desc  create a user
-// @route POST /api/users:id
-// @access private
-const createUser = asyncHandler(async (req, res) => {
-  const { name, email, password, bio } = req.body;
-  if (!name || !email || !password) {
+// @desc get a user data
+// @route GET /api/users/:id
+// @access Public
+const getUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id); //find user by email
+  if (!user) {
     res.status(400);
-    throw new Error("Please add name, email and password");
+    throw new Error("User Not Found");
   }
-  // create a user
-  const createUser = await User.create({
-    name,
-    email,
-    password,
-    bio,
-  });
-  if (createUser) {
-    res.status(201).json(createUser);
-  }
+  res.status(200).json(user);
 });
 
 // @desc  delete a user
-// @route DELETE /api/users:id
+// @route DELETE  /api/users:id
 // @access private
 const deleteUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
@@ -58,4 +88,4 @@ const deleteUser = asyncHandler(async (req, res) => {
   });
 });
 
-export { getUsers, getUser, createUser, deleteUser };
+export { registerUser, loginUser, getUser, deleteUser };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 const router = Router();
 import {
-  createUser,
+  registerUser,
+  loginUser,
   getUser,
-  getUsers,
   deleteUser,
 } from "../controllers/userController.js";
 
 // create crud routes
-router.route("/").post(createUser).get(getUsers);
+router.route("/").post(registerUser);
+router.route("/login").post(loginUser);
 router.route("/:id").get(getUser).delete(deleteUser);
 
 export default router;
